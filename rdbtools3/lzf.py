62c8405f2ec42e08a8b4d22f4069b6a8e8e8c96c
Alexey Popravka <a.popravka@insollo.com>
2013-02-18 14:21:00 +0200

    LZF unpacker fixed

--- rdbtools3/lzf.py
+++ rdbtools3/lzf.py
@@ -11,16 +11,17 @@ def unpack_lzf(f, clen, explen):
         if ctrl < 32:
             out.extend(f.read(ctrl + 1))
             idx += ctrl + 1
-            out_idx += ctrl + 1
+            out_idx = len(out)
         else:
             len_ = ctrl >> 5
             if len_ == 7:
-                len_ = len_ + read_byte(f)
+                len_ += read_byte(f)
                 idx += 1
             ref = out_idx - ((ctrl & 0x1F) << 8) - read_byte(f) - 1
             idx += 1
-            out.extend(out[ref:ref + len_ + 2])
-            out_idx += len_ + 2
+            for i in range(ref, ref + len_ + 2):
+                out.append(out[i])
+            out_idx = len(out)
     if len(out) != explen:
         raise ValueError('Invalid length {}, expected {}'
                          .format(len(out), explen))
