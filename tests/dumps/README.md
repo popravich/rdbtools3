a53507046ccc6baef2807927e429eeb619d787de
Alexey Popravka <alexey.popravka@horsedevel.com>
2013-02-23 01:49:59 +0200

    tests updated; redis dump changed

--- tests/dumps/README.md
+++
@@ -1,1 +0,0 @@
-dumps are taken from redis repository [antirez/redis](https://github.com/antirez/redis)
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -10,14 +10,14 @@ class TestSimple(unittest.TestCase):
     maxDiff = None
 
     def setUp(self):
-        self.rdb = load_dump('encodings.rdb')
+        self.rdb = load_dump('misc.rdb')
 
     def tearDown(self):
         del self.rdb
 
     def test_parse(self):
         ret = list(parse_rdb_stream(self.rdb))
-        self.assertEqual(len(ret), 13)
+        self.assertEqual(len(ret), 9)
         return ret
 
     def testSimple(self):
@@ -25,36 +25,46 @@ class TestSimple(unittest.TestCase):
 
         expected = [
         #    db, type, name, value, expire, extra info
-            (0, 'zset', b'zset', mock.ANY, None, {}),
-            (0, 'set', b'set_zipped_1', mock.ANY, None, {}),
-            (0, 'list', b'list_zipped', mock.ANY, None, {}),
-            (0, 'string', b'string', mock.ANY, None, {}),
-            (0, 'string', b'compressible', mock.ANY, None, {}),
-            (0, 'set', b'set_zipped_2', mock.ANY, None, {}),
-            (0, 'string', b'number', mock.ANY, None, {}),
-            (0, 'set', b'set_zipped_3', mock.ANY, None, {}),
-            (0, 'set', b'set', mock.ANY, None, {}),
-            (0, 'list', b'list', mock.ANY, None, {}),
-            (0, 'hash', b'hash_zipped', mock.ANY, None, {}),
-            (0, 'zset', b'zset_zipped', mock.ANY, None, {}),
-            (0, 'hash', b'hash', mock.ANY, None, {}),
+            (1, 'string', b'str2', mock.ANY, None, mock.ANY),
+            (1, 'hash', b'hset1', mock.ANY, None, mock.ANY),
+            (1, 'set', b'set1', mock.ANY, None, mock.ANY),
+            (1, 'hash', b'some_long_hash_key_name', mock.ANY, None, mock.ANY),
+            (1, 'zset', b'zset1', mock.ANY, None, mock.ANY),
+            (1, 'string', 1, mock.ANY, None, mock.ANY),
+            (1, 'string', b'str1', mock.ANY, None, mock.ANY),
+            (2, 'string', b'str3_exp', mock.ANY, mock.ANY, mock.ANY),
+            (2, 'list', b'list1', mock.ANY, None, mock.ANY),
             ]
         self.assertEqual(expected, items)
 
+    def testKeyValue_str2(self):
+        item = self.test_parse()[0]
+        self.assertEqual(b'str2', item.key)
+        self.assertEqual(123456789, item.value)
+        self.assertEqual({
+            'encoding': 'raw',
+            }, item.info)
+        self.assertIsNone(item.expire)
+
+    def testKeyValue_hset1(self):
+        item = self.test_parse()[1]
+        self.assertEqual(b'hset1', item.key)
+        self.assertEqual([
+            (b'aa', b'10'),
+            ], item.value)
+        self.assertEqual({
+            'encoding': 'zipmap',
+            }, item.info)
+
     def testKeyValue_zset(self):
         # FIXME: must be int/float
-        item = self.test_parse()[0]
+        item = self.test_parse()[4]
+        self.assertEqual(b'zset1', item.key)
         self.assertEqual([
-            (b'bb', b'20'),
-            (b'cc', b'30'),
-            (b'bbb', b'200'),
-            (b'bbbb', b'5000000000'),
-            (b'ccc', b'300'),
-            (b'cccc', b'123456789'),
-            (b'a', b'1'),
-            (b'aa', b'10'),
-            (b'b', b'2'),
-            (b'aaa', b'100'),
-            (b'c', b'3'),
-            (b'aaaa', b'1000'),
+            (b'one', 1),
+            (b'two', 2),
             ], item.value)
+        self.assertEqual({
+            'encoding': 'ziplist',
+            }, item.info)
+        self.assertIsNone(item.expire)
