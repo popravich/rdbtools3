ee9217c95092013aa94bc2e32a71a8ff175a3369
Alexey Popravka <alexey.popravka@horsedevel.com>
2013-02-23 02:25:04 +0200

    few more tests

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -39,6 +39,7 @@ class TestSimple(unittest.TestCase):
 
     def testKeyValue_str2(self):
         item = self.test_parse()[0]
+        self.assertEqual(1, item.dbnum)
         self.assertEqual(b'str2', item.key)
         self.assertEqual(123456789, item.value)
         self.assertEqual({
@@ -48,6 +49,7 @@ class TestSimple(unittest.TestCase):
 
     def testKeyValue_hset1(self):
         item = self.test_parse()[1]
+        self.assertEqual(1, item.dbnum)
         self.assertEqual(b'hset1', item.key)
         self.assertEqual([
             (b'aa', b'10'),
@@ -55,10 +57,37 @@ class TestSimple(unittest.TestCase):
         self.assertEqual({
             'encoding': 'zipmap',
             }, item.info)
+        self.assertIsNone(item.expire)
+
+    def testKeyValue_set1(self):
+        item = self.test_parse()[2]
+        self.assertEqual(1, item.dbnum)
+        self.assertEqual(b'set1', item.key)
+        self.assertEqual([
+            1, 2,
+            ], item.value)
+        self.assertEqual({
+            'encoding': 'intset',
+            }, item.info)
+        self.assertIsNone(item.expire)
+
+    def testKeyValue_long_hash(self):
+        item = self.test_parse()[3]
+        self.assertEqual(1, item.dbnum)
+        self.assertEqual(b'some_long_hash_key_name', item.key)
+        self.assertEqual([
+            (b'even_more_longer_hash_key_field_name',
+             b'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),
+            ], item.value)
+        self.assertEqual({
+            'encoding': 'zipmap',
+            }, item.info)
+        self.assertIsNone(item.expire)
 
     def testKeyValue_zset(self):
         # FIXME: must be int/float
         item = self.test_parse()[4]
+        self.assertEqual(1, item.dbnum)
         self.assertEqual(b'zset1', item.key)
         self.assertEqual([
             (b'one', 1),
@@ -68,3 +97,37 @@ class TestSimple(unittest.TestCase):
             'encoding': 'ziplist',
             }, item.info)
         self.assertIsNone(item.expire)
+
+    def testKeyValue_str(self):
+        item = self.test_parse()[5]
+        self.assertEqual(1, item.dbnum)
+        self.assertEqual(1, item.key)
+        self.assertEqual(b'aa', item.value)
+        self.assertEqual({
+            'encoding': 'raw',
+            }, item.info)
+        self.assertIsNone(item.expire)
+
+    def testKeyValue_list(self):
+        item = self.test_parse()[8]
+        self.assertEqual(2, item.dbnum)
+        self.assertEqual(b'list1', item.key)
+        self.assertEqual([
+            1234567890, b'aaaaaaaaaaaaaaaaaaaaaa', 2, 1
+            ], item.value)
+        self.assertEqual({
+            'encoding': 'ziplist',
+            }, item.info)
+        self.assertIsNone(item.expire)
+
+    def testKeyValue_str_w_expire(self):
+        from datetime import datetime
+
+        item = self.test_parse()[7]
+        self.assertEqual(2, item.dbnum)
+        self.assertEqual(b'str3_exp', item.key)
+        self.assertEqual({
+            'encoding': 'raw',
+            }, item.info)
+        self.assertEqual(datetime(2013, 2, 22, 23, 22, 31),
+                         item.expire)
